ad2ad92c83959c5451a9a536599b92a964361b6c
Ahmed Ramadan <60488889+Ahmed-Ramadan-Rezk@users.noreply.github.com>
2024-07-09 21:14:24 +0300

    Routes correction api.php

--- routes/api.php
+++ routes/api.php
@@ -6,13 +6,13 @@ use App\Http\Controllers\Api\V1\AuthController;
 
 // User Authentication Api Routes
 Route::group(['prefix' => 'v1', 'namespace' => 'App\Http\Controllers\Api\V1'], function () {
-    Route::post('login', [AuthController::class, 'login']);
-    Route::post('register', [AuthController::class, 'register']);
-    Route::get('logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
-    Route::get('profile', [AuthController::class, 'profile'])->middleware('auth:sanctum');
+    Route::post('/login', [AuthController::class, 'login']);
+    Route::post('/register', [AuthController::class, 'register']);
+    Route::get('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
+    Route::get('/profile', [AuthController::class, 'profile'])->middleware('auth:sanctum');
 });
 
 // Note Api Routes
 Route::group(['prefix' => 'v1', 'namespace' => 'App\Http\Controllers\Api\V1'], function () {
-    Route::apiResource('notes', NoteController::class)->middleware('auth:sanctum');
+    Route::apiResource('/notes', NoteController::class)->middleware('auth:sanctum');
 });
